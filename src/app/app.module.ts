31230fba70587ddda288ff945c5d9071d2942792
“usuariogit config --global user.name “delgado.camilo87 <usuario@example.com>
2023-07-28 03:24:16 -0300

    "commit"

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { TareasComponent } from './components/tareas/tareas.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TareasComponent,
+    NavbarComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
